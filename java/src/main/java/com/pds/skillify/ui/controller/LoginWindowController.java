12bac3e137887aaa26b63fd1723f0cbd2d661c50
Mikael Krüppel <mikaelhenriquekp@gmail.com>
2025-02-06 10:27:10 +0100

    Revert "Vista y controlador"
    This reverts commit d8c4928bbb73e5b9903e911a2af26ecae20c259a.

--- java/src/main/java/com/pds/skillify/ui/controller/LoginWindowController.java
+++ java/src/main/java/com/pds/skillify/ui/controller/LoginWindowController.java
@@ -1,13 +1,6 @@
 package com.pds.skillify.ui.controller;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
-
 import com.pds.skillify.ui.LoginWindow;
-import com.pds.skillify.ui.RegisterWindow;
 
 public class LoginWindowController {
 
@@ -15,42 +8,6 @@ public class LoginWindowController {
 
 	public LoginWindowController(LoginWindow view) {
 		this.view = view;
-		initializeControllers();
-	}
-
-	private void initializeControllers() {
-		handleClickOnRegister(view.getRegisterLabel());
-		handleClickOnForgotPassword(view.getForgotPasswordLabel());
-		handleClickOnLogin(view.getLoginButton());
-	}
-
-	private void handleClickOnRegister(JLabel registerLabel) {
-		registerLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				view.dispose();
-				new RegisterWindow();
-			}
-		});
-	}
-
-	private void handleClickOnForgotPassword(JLabel forgotPasswordLabel) {
-		forgotPasswordLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				//TODO: implementar
-				
-			}
-		});
-	}
-
-	private void handleClickOnLogin(JButton loginButton) {
-		loginButton.addActionListener(e -> {
-			
-			//TODO: implementar
-			
-		});
 	}
 
 }
